6d86caf9cf924037abf841b8b8e9d90454b769b7
Kavya Mavuru <mavuru_k@India.XoriantCorp.com>
2018-08-01 18:08:15 +0530

    added forms in feature

--- src/app/feature/feature.component.ts
+++ src/app/feature/feature.component.ts
@@ -5,11 +5,21 @@ import { Component, OnInit } from '@angular/core';
     templateUrl: './feature.component.html'
 })
 export class FeatureComponent implements OnInit {
+    form: any;
+  powers: string[];
+  submitted: boolean = false;
 
     constructor() { }
 
     ngOnInit() { 
+        this.powers = ['Really Smart', 'Turbulent Breeze', 
+        'Super Hot', 'Weather Changer'];
 
     }
+
+    onSubmit(form: any)  {
+        this.submitted = true;
+        this.form = form;
+      }
 
 }
